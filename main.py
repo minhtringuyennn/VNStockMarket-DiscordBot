fb4936f423b49dd475d5626116f422b5842542f1
Minh Tri Nguyen <54835452+minhtringuyennn@users.noreply.github.com>
2022-01-07 00:12:52 +0700

    Crawl list

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import stock.fetch as dl
 
-import json
+import json, csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mpldates
+from pathlib import Path
 
 # # TODO
 # RSI, MACD, BOILINGER BAND
@@ -36,19 +37,33 @@ steel_inds  = ['HPG', 'HSG', 'NKG', 'POM', 'SMC', 'TIS', 'TLH', 'TVN', 'VCA', 'V
 stocks      = ['AAS', 'AGR', 'APG', 'APS', 'ART', 'BMS', 'BSI', 'BVS', 'CTS', 'DSC', 'EVS', 'FTS', 'HBS', 'HCM', 'IVS', 'MBS', 'ORS', 'PSI', 'SBS', 'SHS', 'SSI', 'TCI', 'TVB', 'TVS', 'VCI', 'VDS', 'VFS', 'VIG', 'VIX', 'VND', 'WSS']
 airline     = ['ACV', 'ASG', 'AST', 'CIA', 'HVN', 'MAS', 'NCS', 'NCT', 'SAS', 'SCS', 'VJC']
 
-loader = dl.FetchCategories()
-data = loader.batch_download(steel_inds)
+# loader = dl.FetchCategories()
+# data = loader.batch_download(steel_inds)
 
-result = data.to_json(orient="table")
-parsed = json.loads(result)
+# result = data.to_json(orient="table")
+# parsed = json.loads(result)
+
+# idx = 0
+# for value in parsed['data']:
+#     value['index'] = idx
+#     idx += 1
+
+# with open('temp_data.json', 'w', encoding='utf-8') as f:
+#     json.dump(parsed, f, ensure_ascii=False, indent=4)
+
+p = Path(r'stock_list.json')
+
+with p.open('r', encoding='utf-8') as f:
+    data = json.loads(f.read())
+
+file_name = ['HOSE_List', 'HNX_List', 'UPCOM_List', 'VN30_List', 'DuocPham_List', 'Cang_List', 'DetMay_List', 'NangLuong_List', 'XayDung_List', 'BDSNHAO_List', 'BDSCN_List', 'ThuySan_List', 'PhanBonHoaChat_List', 'BanLe_List', 'NganHang_List', 'DauKhi_List', 'Thep_List', 'ChungKhoan_List', 'HangKhong_List']
 
 idx = 0
-for value in parsed['data']:
-    value['index'] = idx
+for name in file_name:
+    df = pd.json_normalize(data[idx]['stocks'])
+    df.drop('delistedDate', axis =1, inplace = True)
+    df.to_csv(f'{name}.csv', index=False, encoding='utf-8')
     idx += 1
-
-with open('temp_data.json', 'w', encoding='utf-8') as f:
-    json.dump(parsed, f, ensure_ascii=False, indent=4)
 
 # _MA20 = data['close'][Symbol].rolling(MOVING_AVERAGES_20).mean().tolist()
 # _MA50 = data['close'][Symbol].rolling(MOVING_AVERAGES_50).mean().tolist()
@@ -63,10 +78,10 @@ with open('temp_data.json', 'w', encoding='utf-8') as f:
 # for val in _MA20:
 #     oPrice = data['open'][Symbol][idx]
 #     cPrice = data['close'][Symbol][idx]
-    
+
 #     if oPrice > cPrice and oPrice > val and cPrice < val:
 #         Message("Sell", Symbol, data, idx)
 #     elif oPrice < cPrice and oPrice < val and cPrice > val:
 #         Message("Buy", Symbol, data, idx)
-        
+
 #     idx = idx + 1
